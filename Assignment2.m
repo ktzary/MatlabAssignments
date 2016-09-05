752ba84e1c0c5c8f706f310d5e1c8bdf8511cff3
ktzary <ktzary@gmail.com>
2016-09-05 17:06:28 -0400

    Initial Commit

---
+++ Assignment2.m
@@ -0,0 +1,51 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Assignment 2: Textbook Exercises
+%
+% Submitted by: Kelsey Zary
+%
+% Due: September 15th
+%
+% There are 3 sections to this assignment, one for Chapters 1-3 in the
+% textbook. 
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Chapter 1 Exercises
+%
+% Complete Exercises 1-6, 11-15, 22, 24-27
+%
+% 1.)
+Copper = 63.55;
+
+% 2.)
+myage = 25;
+myage = myage - 2;
+myage = myage + 1;
+
+% 3.)
+A = namelengthmax;
+
+% 4.)
+lbs = 2;
+oz = 32;
+who
+whos
+clear oz
+who
+whos
+
+% 5.)
+min32 = intmin('uint32');
+max32 = intmax('uint32');
+min64 = intmin('uint64');
+max64 = intmax('uint64');
+
+% 6.)
+val6 = 24;
+val6i = int32(val6);
+
+
+
+
+
+
